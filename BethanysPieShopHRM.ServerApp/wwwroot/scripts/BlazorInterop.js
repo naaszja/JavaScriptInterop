738bc7388a8f7d460a2e84acb91495d0c4e7161f
naaszja <naaszja@gmail.com>
2021-09-10 15:58:45 -0500

    Brining work home

---
+++ BethanysPieShopHRM.ServerApp/wwwroot/scripts/BlazorInterop.js
@@ -0,0 +1,34 @@
+﻿var blazorInterop = blazorInterop || {};
+
+blazorInterop.showAlert = function showAlert(message) {
+    alert(message)
+};
+
+blazorInterop.logToConsoleTable = function (obj) {
+    console.table(obj);
+}
+
+blazorInterop.showPrompt = function (message, defaultValue) {
+    return prompt(message, defaultValue)
+}
+
+blazorInterop.createEmployee = function (firstName, lastName) {
+    return {
+        firstName,
+        lastName,
+        email: firstName + "@test.com",
+    };
+}
+
+blazorInterop.focusElement = function (element) {
+    element.focus();
+}
+
+blazorInterop.focusElementById = function (id) {
+    var element = document.getElementById(id)
+    if (element) element.focus();
+}
+
+blazorInterop.throwsError = function () {
+    throw Error("function was not implemented");
+}
